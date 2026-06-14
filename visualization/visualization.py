0fd0fecb393d46c86594ca94ee3e25023d85774d
syifa-azzah <nursyifaaz_bs23@iluv.ums.edu.my>
2026-06-14 21:07:53 +0000

    Fix visualization output

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -2,18 +2,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def plot_histograms(df):
-
     df.hist(figsize=(12, 10))
-
     plt.tight_layout()
-
-    plt.show()
+    plt.savefig("histogram.png")
+    plt.close()
 
 
 def plot_heatmap(df):
-
     plt.figure(figsize=(10, 8))
-
     sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
-
-    plt.show()
+    plt.savefig("heatmap.png")
+    plt.close()
